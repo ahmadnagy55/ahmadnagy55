79c21f44be8489334cf9dfb1789ce4991b6ca01e
Ahmad Nagy <ahmadnagy55@gmail.com>
2021-11-21 15:37:14 +0200

    'Add final change'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import FilteredData from './component/FilterData/FilteredData;';
 import JobItems from './component/JobItems/JobItems';
 import bgHeaderDeskTop from './component/JobItems/images/bg-header-desktop.svg';
 
@@ -11,7 +10,6 @@ function App() {
       <header className="App-header">
         <img src={bgHeaderDeskTop} className="App-logo" alt="bg-header-desktop" />
         </header>
-        {/* <FilteredData /> */}
         <JobItems/>
     </div>
     
--- src/component/JobItems/JobItems.js
+++ src/component/JobItems/JobItems.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import datalist from '../../data.json';
 import './JobItems.css';
 
-//console.log(datalist[0].logo)
-// async function get () {
-//   let res = await fetch('../../data.json')
-//   let data = await res.json();
-//   console.log(data)
-// }
-// get();
+
 function JobItems () {
+  let [data, setData]= useState(datalist);
+  // console.log (datalist);
   let handelClear =  () => {
+    //remove selected language in banner
+    document.querySelector('.dataType').innerHTML="";
+    // hide banner
     document.querySelector(".filteredBar").classList.add("disable");
+    setData(datalist) ;
+    
+  }
+  let handelClick = (e) => {
+    console.log (e.target);
+    //show banner tags
+    document.querySelector(".filteredBar").classList.remove("disable");
+    //display selected language in banner
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'r-item');
+    newDiv.appendChild(document.createTextNode(e.target.innerText))
+    document.querySelector('.dataType').appendChild(newDiv);
+    //map on data array & display matched data
+    let currentData = [...datalist]
+    let newData = currentData.filter((data) =>
+    (data.role.toLowerCase().includes(e.target.innerText.toLowerCase()) ||
+    data.level.toLowerCase().includes(e.target.innerText.toLowerCase()) //||
+    
+    )   
+    );
+    setData(newData);
   }
   return ( 
     <div>
-      <div className="filteredBar"> 
-        <div className='dataType'>Filtered Data</div>
+      <div className="filteredBar disable"> 
+        <div className='dataType'></div>
         <div className='clear' onClick={handelClear}>Clear</div>
       </div>
       <div className="JobItemsContainer">
-      {datalist.map(item => (
+      {data.map(item => (
                 <div key={item.id} className={item.new && item.featured ? "JobItemList pseudo" : "JobItemList"}>
                   <div className='LeftItem'>
                       <img className="logo" src={item.logo} alt={item.company}/>
@@ -38,16 +58,16 @@ function JobItems () {
                       </ul>
                         </div>
                         </div>
-                      </div>
+                  </div>
                       <div className="break-line"></div>
-                      <div className="RightItem">
+                  <div className="RightItem">
                         <div className='container'>
-                        <div className="r-item role">{item.role}</div>
-                        <div className="r-item level">{item.level}</div>
-                        {item.languages? item.languages.map(lang => <div key={Math.random()} className="r-item languages0">{lang}</div>) : "" }
-                        {item.tools? item.tools.map(tool =><div key={Math.random()} className="r-item tools">{tool}</div> ) : ""}
+                        <div className="r-item role" onClick={handelClick}> {item.role} </div>
+                        <div className="r-item level" onClick={handelClick}> {item.level} </div>
+                        {item.languages? item.languages.map(lang => <div key={Math.random()} className="r-item languages0" onClick={handelClick}> {lang} </div> ) : "" }
+                        {item.tools? item.tools.map(tool => <div key={Math.random()} className="r-item tools" onClick={handelClick}> {tool} </div> ) : ""}
                         </div>
-                        </div>
+                  </div>
                 
                 </div>
                 
